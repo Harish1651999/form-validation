af78a909ea42c53580eded294a2583def562047c
S.Harishkumar <hk1651999@gmail.com>
2021-02-16 20:34:58 +0530

    loginpage

---
+++ js/main.js
@@ -0,0 +1,9 @@
+    a = document.getElementById('fname').value;
+    b = document.getElementById('pwd').value;
+    validate(a,b);
+
+function validate(a,b) {
+    if(a = ""){
+        alert('please fill the field');
+    }
+}
