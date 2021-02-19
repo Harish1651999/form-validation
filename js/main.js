047ad1d25b45f8f24670ebaf09b6da5dca24dbf4
S.Harishkumar <hk1651999@gmail.com>
2021-02-19 06:36:31 +0530

    functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-    a = document.getElementById('fname').value;
-    b = document.getElementById('pwd').value;
-    validate(a,b);
-
-function validate(a,b) {
-    if(a = ""){
-        alert('please fill the field');
+function validate(){
+    let userName = document.getElementById('name').value;
+    let emailAddr = document.getElementById('mail').value;
+    let mobileNum = document.getElementById('phone').value;
+    let passWord = document.getElementById('pwd').value;
+    
+    if(userName == ""){
+        alert('Please enter your name')
     }
-}
+    
+    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if(!reg.test(emailAddr)){
+        alert('Invalid email');
+    }
+    
+    if((mobileNum.length != 10) || (Number.isInteger(mobileNum)))
+    {
+        alert('Enter correct mobile number');
+    }
+    
+    if(passWord.length <= 6){
+        alert('Minimum 6 characters');
+    }
+    
+    if(passWord.length >= 18){
+        alert('Maximum 18 characters');
+    }
+    // document.getElementById('name-value').textContent = userName;
+    }
