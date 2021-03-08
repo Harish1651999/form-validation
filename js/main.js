8127c98f649a1686a64eb43a2946ef08bb0ba26e
S.Harishkumar <hk1651999@gmail.com>
2021-03-08 13:42:41 +0530

    Regex Added

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ function validate(){
     let emailAddr = document.getElementById('mail').value;
     let mobileNum = document.getElementById('phone').value;
     let passWord = document.getElementById('pwd').value;
+    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let regpass = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    let regnum = /^([+]\d{2}[])?\d{10}$/;
     let nameValue = document.getElementById("name-value");
-    
+    let mailValue = document.getElementById("mail-value");
+    let mobileValue = document.getElementById("mobile-value");
+    let passwordValue = document.getElementById("password-value");
+
+    // NAME
     if (userName == "") {
         nameValue.style.display = "block";
         setTimeout(function () {
@@ -12,28 +19,44 @@ function validate(){
         }, 6000);
       }
     
-    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    if(!reg.test(emailAddr)){
-        alert('Invalid email');
+    // EMAIL
+    else if(!reg.test(emailAddr)){
+        mailValue.style.display = "block";
+        setTimeout(function () {
+            mailValue.style.display = "none";
+        }, 6000);
     }
     
-    if((mobileNum.length != 10) || (Number.isInteger(mobileNum)))
+    // NUMBER  mobileNum.length != 10
+    else if((!regnum.test(mobileNum)) || (Number.isInteger(mobileNum)))
     {
-        alert('Enter correct mobile number');
+        mobileValue.style.display = "block";
+        setTimeout(function () {
+            mobileValue.style.display ="none";
+        }, 6000);
     }
     
-    if(passWord.length <= 6){
-        alert('Minimum 6 characters');
+    // PASSWORD
+    else if(!regpass.test(passWord)){
+        passwordValue.style.display = "block";
+        setTimeout(function (){
+        passwordValue.style.display = "none"; 
+        }, 6000);
     }
+    // else if((passWord.length <= 6) || (passWord.length >= 18)) {
+    //     alert('Minimum 6 characters');
+    // }
     
-    if(passWord.length >= 18){
-        alert('Maximum 18 characters');
+    // else if(passWord.length >= 18){
+    //     alert('Maximum 18 characters');
+    // } 
+    else {
+        alert('Successfully logged in')
+        document.getElementById("name").value = "";
+        document.getElementById("mail").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("pwd").value = "";
     }
-    
-    // setTimeout(timer(){
-
-    // },3000);
-    // document.getElementById('name-value').innerText  = "Please enter your name";
     }
 
     
